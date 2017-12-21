69f6323290dd9c55b69f28bb125ae4e40cb901c3
Christopher Sanson <csanson@google.com>
2017-12-21 10:46:23 -0500

    chrome fix attempt

--- karma.conf.js
+++ karma.conf.js
@@ -135,4 +135,7 @@ module.exports = function (config) {
     config.singleRun = true;
     config.browserNoActivityTimeout = 90000;
   }
+  config.browsers = ['Chrome_travis_ci'];
+  config.singleRun = true;
+  config.browserNoActivityTimeout = 90000;
 };
